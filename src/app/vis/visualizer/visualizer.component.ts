83535bcb293207ae8d7e5d1fb2e5207051c4c87f
el-davo <david.ahern26@gmail.com>
2021-02-27 15:41:40 +0000

    Updated

--- src/app/vis/visualizer/visualizer.component.ts
+++ src/app/vis/visualizer/visualizer.component.ts
@@ -1,5 +1,5 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import * as THREE from 'three';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import {VisService} from '../vis.service';
@@ -22,13 +22,13 @@ export class VisualizerComponent implements AfterViewInit {
   constructor(private visService: VisService) {
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 500);
     this.camera.position.z = 1000;
-    this.scene.add(new THREE.AmbientLight(0x916262));
-    this.scene.add(new THREE.HemisphereLight(0xffffbb, 0x080820, 1));
   }
 
   ngAfterViewInit(): void {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.renderer.setClearColor(0xffffff);
+    this.scene.add(new THREE.AmbientLight(0x916262, 0.5));
+    this.scene.add(new THREE.HemisphereLight(0xffffbb, 0x080820, 1));
 
     this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
     this.renderer.setSize(this.container.nativeElement.offsetWidth, this.container.nativeElement.offsetHeight);
